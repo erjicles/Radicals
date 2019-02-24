45142a9208e42976e90d319de29b40b9b6c23fb6
erjicles <erjicles@gmail.com>
2019-02-24 13:05:44 -0600

    Initial commit

---
+++ Radicals/Base.cs
@@ -0,0 +1,117 @@
+﻿using Open.Numeric.Primes;
+using Rationals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace Radicals
+{
+    public partial struct Radical
+    {
+
+        // R = c * sqrt(r)
+        public Rational C { get; }
+        public BigInteger R { get; }
+
+        
+
+        public Radical(Rational fractionalRadical)
+            : this(fractionalRadical.Denominator, fractionalRadical.Numerator * fractionalRadical.Denominator)
+        {
+        }
+
+        public Radical(ref Rational c, ref BigInteger r)
+        {
+            if (r < 0)
+                throw new InvalidOperationException("Negative value under radical");
+            C = c;
+            R = r;
+        }
+
+        public Radical(Rational c, BigInteger r)
+        {
+            if (r < 0)
+                throw new InvalidOperationException("Negative value under radical");
+            C = c;
+            R = r;
+        }
+
+        /// <summary>
+        /// Simplest form is irreducible radical where the radical is the smallest possible integer
+        /// </summary>
+        public Radical SimplestForm
+        {
+            get
+            {
+                if (C.IsZero)
+                    return Zero;
+                if (R.IsZero)
+                    return Zero;
+
+                List<BigInteger> perfectSquareFactors = new List<BigInteger>();
+                List<BigInteger> finalFactors = new List<BigInteger>();
+                var currentCount = 0;
+                BigInteger currentFactor = 0;
+                foreach (BigInteger factor in Prime.Factors(R).OrderBy(f => f))
+                {
+                    if (factor != currentFactor)
+                    {
+                        if (currentCount > 0)
+                            finalFactors.Add(currentFactor);
+                        currentCount = 0;
+                        currentFactor = factor;
+                    }
+                    currentCount++;
+                    if (currentCount == 2)
+                    {
+                        perfectSquareFactors.Add(currentFactor);
+                        currentCount = 0;
+                        currentFactor = 0;
+                    }
+                    else if (currentCount > 2)
+                        throw new Exception("This shouldn't happen");
+                }
+                if (currentCount == 1)
+                    finalFactors.Add(currentFactor);
+                else if (currentCount == 2)
+                    throw new Exception("This should not happen");
+
+                Rational simplestCoefficient = C;
+                BigInteger simplestRadical = 1;
+
+                foreach (BigInteger perfectSquareFactor in perfectSquareFactors)
+                    simplestCoefficient *= perfectSquareFactor;
+                foreach (BigInteger factor in finalFactors)
+                    simplestRadical *= factor;
+
+                var simplest = new Radical(simplestCoefficient.CanonicalForm, simplestRadical);
+                return simplest;
+            }
+        }
+
+        public override string ToString()
+        {
+            if (C.IsZero)
+                return "0";
+            if (R.IsZero)
+                return "0";
+
+            string cPart = "";
+            string rPart = "";
+            if (!C.IsOne)
+                cPart = C.ToString();
+            if (!R.IsOne)
+                rPart = "Sqrt(" + R.ToString() + ")";
+            if (cPart.Length > 0 && rPart.Length > 0)
+            {
+                if (C.Denominator == 1)
+                    cPart = cPart + " * ";
+                else
+                    cPart = "(" + cPart + ") * ";
+            }
+            return cPart + rPart;
+        }
+
+    }
+}
---
+++ Radicals/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Radicals
+{
+    public partial struct Radical
+    {
+        /// <summary>
+        /// Zero
+        /// </summary>
+        public static readonly Radical Zero = new Radical(0, 0);
+
+        /// <summary>
+        /// One
+        /// </summary>
+        public static readonly Radical One = new Radical(1, 1);
+    }
+}
