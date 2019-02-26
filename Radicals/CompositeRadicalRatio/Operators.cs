ede3d67e2b9b24e66e78d0924ff2bf53db087468
erjicles <erjicles@gmail.com>
2019-02-25 22:06:15 -0600

    Added operators to CompositeRadicalRatio

--- Radicals/CompositeRadicalRatio/Operators.cs
+++ Radicals/CompositeRadicalRatio/Operators.cs
@@ -6,9 +6,58 @@ namespace Radicals
 {
     public readonly partial struct CompositeRadicalRatio
     {
+        public static bool operator <(CompositeRadicalRatio left, CompositeRadicalRatio right)
+        {
+            return left.CompareTo(right) < 0;
+        }
+
+        public static bool operator <=(CompositeRadicalRatio left, CompositeRadicalRatio right)
+        {
+            return left.CompareTo(right) <= 0;
+        }
+
+        public static bool operator >(CompositeRadicalRatio left, CompositeRadicalRatio right)
+        {
+            return left.CompareTo(right) > 0;
+        }
+        public static bool operator >=(CompositeRadicalRatio left, CompositeRadicalRatio right)
+        {
+            return left.CompareTo(right) >= 0;
+        }
+
+        public static bool operator ==(CompositeRadicalRatio left, CompositeRadicalRatio right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(CompositeRadicalRatio left, CompositeRadicalRatio right)
+        {
+            return !left.Equals(right);
+        }
+
         public static CompositeRadicalRatio operator -(CompositeRadicalRatio value)
         {
             return Negate(value);
+        }
+
+        public static CompositeRadicalRatio operator +(CompositeRadicalRatio value)
+        {
+            return value;
+        }
+
+        public static CompositeRadicalRatio operator +(CompositeRadicalRatio left, CompositeRadicalRatio right)
+        {
+            return Add(left, right);
+        }
+
+        public static CompositeRadicalRatio operator -(CompositeRadicalRatio left, CompositeRadicalRatio right)
+        {
+            return Subtract(left, right);
+        }
+
+        public static CompositeRadicalRatio operator *(CompositeRadicalRatio left, CompositeRadicalRatio right)
+        {
+            return Multiply(left, right);
         }
     }
 }
