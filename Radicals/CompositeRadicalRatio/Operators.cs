9cb52bc257e4d129e33457c37738d2f4fe55790c
erjicles <erjicles@gmail.com>
2019-02-25 23:15:08 -0600

    Added operators for CompositeRadicalRatio for Rational

--- Radicals/CompositeRadicalRatio/Operators.cs
+++ Radicals/CompositeRadicalRatio/Operators.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rationals;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -45,19 +46,67 @@ namespace Radicals
             return value;
         }
 
-        public static CompositeRadicalRatio operator +(CompositeRadicalRatio left, CompositeRadicalRatio right)
+        public static CompositeRadicalRatio operator +(
+            CompositeRadicalRatio left, 
+            CompositeRadicalRatio right)
         {
             return Add(left, right);
         }
 
-        public static CompositeRadicalRatio operator -(CompositeRadicalRatio left, CompositeRadicalRatio right)
+        public static CompositeRadicalRatio operator +(
+            CompositeRadicalRatio left,
+            Rational right)
+        {
+            return Add(left, new CompositeRadicalRatio(right, 1));
+        }
+
+        public static CompositeRadicalRatio operator +(
+            Rational left,
+            CompositeRadicalRatio right)
+        {
+            return Add(new CompositeRadicalRatio(left, 1), right);
+        }
+
+        public static CompositeRadicalRatio operator -(
+            CompositeRadicalRatio left, 
+            CompositeRadicalRatio right)
         {
             return Subtract(left, right);
         }
 
-        public static CompositeRadicalRatio operator *(CompositeRadicalRatio left, CompositeRadicalRatio right)
+        public static CompositeRadicalRatio operator -(
+            CompositeRadicalRatio left,
+            Rational right)
+        {
+            return Subtract(left, new CompositeRadicalRatio(right, 1));
+        }
+
+        public static CompositeRadicalRatio operator -(
+            Rational left,
+            CompositeRadicalRatio right)
+        {
+            return Subtract(new CompositeRadicalRatio(left, 1), right);
+        }
+
+        public static CompositeRadicalRatio operator *(
+            CompositeRadicalRatio left, 
+            CompositeRadicalRatio right)
         {
             return Multiply(left, right);
+        }
+
+        public static CompositeRadicalRatio operator *(
+            CompositeRadicalRatio left, 
+            Rational right)
+        {
+            return Multiply(left, new CompositeRadicalRatio(right, 1));
+        }
+
+        public static CompositeRadicalRatio operator *(
+            Rational left,
+            CompositeRadicalRatio right)
+        {
+            return Multiply(new CompositeRadicalRatio(left, 1), right);
         }
     }
 }
