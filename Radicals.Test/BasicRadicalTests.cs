ee7f91ddb211ac68a5d318cb9a6370dc427f78f2
erjicles <erjicles@gmail.com>
2019-02-26 15:24:52 -0600

    Added multiplication tests for BasicRadical

--- Radicals.Test/BasicRadicalTests.cs
+++ Radicals.Test/BasicRadicalTests.cs
@@ -117,5 +117,51 @@ namespace Radicals.Test
             Assert.Equal(expected4, actual4);
             Assert.Equal(expected5, actual5);
         }
+
+        [Fact]
+        public void MultiplicationTests()
+        {
+            // (3*sqrt(2)) * ((5/3)*sqrt(3)) = (15/3)*sqrt(6) = 5*sqrt(6)
+            var b11 = new BasicRadical(3, 2);
+            var b12 = new BasicRadical(new Rational(5, 3), 3);
+            var actual1 = b11 * b12;
+            var expected1 = new BasicRadical(5, 6);
+            // 11 * sqrt(4/9) = 22/3
+            var b21 = new BasicRadical(11, 1);
+            var b22 = new BasicRadical(new Rational(4, 9));
+            var actual2 = b21 * b22;
+            var expected2 = new BasicRadical(new Rational(22,3), 1);
+            // 11 * sqrt(4/9) = 22/3
+            var b31 = 11;
+            var b32 = new BasicRadical(new Rational(4, 9));
+            var actual3 = b31 * b32;
+            var expected3 = new BasicRadical(new Rational(22, 3), 1);
+            // [(3/2)*sqrt(2) - (7/3)*sqrt(5) + (1/3)*sqrt(2) - (7/5)*sqrt(5)] * (11/4)*sqrt(2)
+            // = [(11/6)*sqrt(2) - (56/15)*sqrt(5)] * (11/4)*sqrt(2)
+            // = (121/24)*2 - (154/15)*sqrt(10)
+            // = (121/12) - (56/15)*sqrt(10)
+            var b41 = new BasicRadical(new Rational(3, 2), 2);
+            var b42 = new BasicRadical(new Rational(7, 3), 5);
+            var b43 = new BasicRadical(new Rational(1, 3), 2);
+            var b44 = new BasicRadical(new Rational(7, 5), 5);
+            var b45 = new BasicRadical(new Rational(11, 4), 2);
+            var actual41 = (b41 - b42 + b43 - b44) * b45;
+            var actual42 = b45 * (b41 - b42 + b43 - b44);
+            var expected4 = new BasicRadical[2] {
+                new BasicRadical(new Rational(121, 12), 1),
+                new BasicRadical(new Rational(-154, 15), 10) };
+
+            Assert.Equal(expected1, actual1);
+            Assert.Equal(expected2, actual2);
+            Assert.Equal(expected3, actual3);
+            Assert.Equal(expected4, actual41);
+            Assert.Equal(expected4, actual42);
+        }
+
+        [Fact]
+        public void DivisionTests()
+        {
+
+        }
     }
 }
