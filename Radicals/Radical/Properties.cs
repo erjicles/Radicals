2a0af8aba09dc2f5f13af5a3a85acf01b4d81fd5
erjicles <erjicles@gmail.com>
2019-03-04 22:55:37 -0600

    Added IsRational function to Radical and RadicalSum

---
+++ Radicals/Radical/Properties.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Radicals
+{
+    public readonly partial struct Radical
+    {
+        public bool IsRational()
+        {
+            if (Radicand < 2)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ Radicals/RadicalSum/Properties.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Radicals
+{
+    public readonly partial struct RadicalSum
+    {
+        public bool IsRational()
+        {
+            for (int i = 0; i < Radicals.Length; i++)
+                if (Radicals[i].Radicand > 1)
+                    return false;
+            return true;
+        }
+    }
+}
