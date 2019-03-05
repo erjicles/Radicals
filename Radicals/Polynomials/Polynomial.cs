80eb482298c75e6a887a889a15590d10b815995a
erjicles <erjicles@gmail.com>
2019-03-04 22:59:59 -0600

    Added polynomial helper classes for rationalizing radical sums

---
+++ Radicals/Polynomials/Polynomial.cs
@@ -0,0 +1,278 @@
+﻿using Open.Numeric.Primes;
+using Rationals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace Radicals.Polynomials
+{
+    internal readonly struct Polynomial
+    {
+        private readonly PolynomialTerm[] _terms;
+        public PolynomialTerm[] Terms
+        {
+            get
+            {
+                if (_terms == null)
+                    return new PolynomialTerm[1] { PolynomialTerm.Zero };
+                return _terms;
+            }
+        }
+
+        public Polynomial(RadicalSum constant)
+            : this(new PolynomialTerm[1] { new PolynomialTerm(constant)})
+        {
+        }
+
+        public Polynomial(PolynomialTerm term)
+            : this(new PolynomialTerm[1] { term })
+        {
+        }
+
+        public Polynomial(PolynomialTerm[] terms)
+        {
+            if (terms == null)
+                throw new ArgumentNullException(nameof(terms));
+            ToSimplestForm(terms_in: terms, terms_out: out _terms);
+        }
+
+        private static void ToSimplestForm(PolynomialTerm[] terms_in, out PolynomialTerm[] terms_out)
+        {
+            var termDictionary = new Dictionary<int, PolynomialTerm>();
+            for (int i = 0; i < terms_in.Length; i++)
+            {
+                var degree = terms_in[i].Degree;
+                if (termDictionary.ContainsKey(degree))
+                {
+                    var currentTerm = termDictionary[degree];
+                    termDictionary[degree] = new PolynomialTerm(currentTerm.Coefficient + terms_in[i].Coefficient, degree);
+                }
+                else if (terms_in[i] != PolynomialTerm.Zero)
+                {
+                    termDictionary.Add(terms_in[i].Degree, terms_in[i]);
+                }
+            }
+            var result = 
+                termDictionary.Values
+                .Where(t => t != PolynomialTerm.Zero)
+                .OrderBy(t => t.Degree)
+                .ToArray();
+            if (result.Length == 0)
+                result = new PolynomialTerm[1] { PolynomialTerm.Zero };
+            terms_out = result;
+        }
+
+        public void ExtractTermsContainingRadicand(BigInteger radicand, out Polynomial p_reduced, out Polynomial p_extract)
+        {
+            p_reduced = Polynomial.Zero;
+            p_extract = Polynomial.Zero;
+            for (int i = 0; i < Terms.Length; i++)
+            {
+                var coeff = Terms[i].Coefficient;
+                RadicalSum coeff_reduced = coeff;
+                RadicalSum coeff_target = RadicalSum.Zero;
+                for (int j = 0; j < coeff.Radicals.Length; j++)
+                {
+                    if (coeff.Radicals[j].Radicand % radicand == 0)
+                    {
+                        coeff_target += coeff.Radicals[j];
+                        coeff_reduced -= coeff.Radicals[j];
+                    }
+                }
+                p_reduced += new PolynomialTerm(coeff_reduced, Terms[i].Degree);
+                p_extract += new PolynomialTerm(coeff_target, Terms[i].Degree);
+            }
+        }
+
+        public RadicalSum GetConstantTerm()
+        {
+            for (int i = 0; i < Terms.Length; i++)
+                if (Terms[i].Degree == 0)
+                    return Terms[i].Coefficient;
+            return RadicalSum.Zero;
+        }
+
+        public BigInteger[] GetUniquePrimeRadicands()
+        {
+            var foundRadicands = new HashSet<BigInteger>();
+            for (int i = 0; i < Terms.Length; i++)
+            {
+                for (int j = 0; j < Terms[i].Coefficient.Radicals.Length; j++)
+                {
+                    var r = Terms[i].Coefficient.Radicals[j];
+                    foreach (BigInteger primeFactor in Prime.Factors(r.Radicand))
+                    {
+                        if (!foundRadicands.Contains(primeFactor))
+                            foundRadicands.Add(primeFactor);
+                    }
+                }
+            }
+            return foundRadicands.OrderBy(r => r).ToArray();
+        }
+
+        public BigInteger GetLargestRadicand()
+        {
+            BigInteger largestRadicand = 0;
+            for (int i = 0; i < Terms.Length; i++)
+            {
+                for (int j = 0; j < Terms[i].Coefficient.Radicals.Length; j++)
+                {
+                    if (Terms[i].Coefficient.Radicals[j].Radicand > largestRadicand)
+                        largestRadicand = Terms[i].Coefficient.Radicals[j].Radicand;
+                }
+            }
+            return largestRadicand;
+        }
+
+        public bool IsDivisibleByX
+        {
+            get
+            {
+                for (int i = 0; i < Terms.Length; i++)
+                {
+                    if (Terms[i].Degree == 0)
+                        return false;
+                }
+                return true;
+            }
+        }
+
+        public static Polynomial Zero = new Polynomial(new PolynomialTerm[1] { PolynomialTerm.Zero });
+        public static Polynomial One = new Polynomial(new PolynomialTerm[1] { PolynomialTerm.One });
+
+        public static Polynomial Negate(Polynomial value)
+        {
+            var terms = new PolynomialTerm[value.Terms.Length];
+            for (int i = 0; i < value.Terms.Length; i++)
+                terms[i] = -value.Terms[i];
+            var result = new Polynomial(terms);
+            return result;
+        }
+
+        public static Polynomial Add(Polynomial left, Polynomial right)
+        {
+            var terms = new PolynomialTerm[left.Terms.Length + right.Terms.Length];
+            left.Terms.CopyTo(terms, 0);
+            right.Terms.CopyTo(terms, left.Terms.Length);
+            var result = new Polynomial(terms);
+            return result;
+        }
+
+        public static Polynomial Subtract(Polynomial left, Polynomial right)
+        {
+            return Add(left, -right);
+        }
+
+        public static Polynomial Multiply(Polynomial left, Polynomial right)
+        {
+            var terms = new PolynomialTerm[left.Terms.Length * right.Terms.Length];
+            for (int i = 0; i < left.Terms.Length; i++)
+                for (int j = 0; j < right.Terms.Length; j++)
+                    terms[(i * right.Terms.Length) + j] = left.Terms[i] * right.Terms[j];
+            return new Polynomial(terms);
+        }
+
+        public static Polynomial Divide(Polynomial left, Radical right)
+        {
+            var terms = new PolynomialTerm[left.Terms.Length];
+            for (int i = 0; i < left.Terms.Length; i++)
+                terms[i] = new PolynomialTerm(left.Terms[i].Coefficient / right, left.Terms[i].Degree);
+            return new Polynomial(terms);
+        }
+
+        public static Polynomial DivideByX(Polynomial value)
+        {
+            var terms = new PolynomialTerm[value.Terms.Length];
+            for (int i = 0; i < value.Terms.Length; i++)
+                terms[i] = new PolynomialTerm(value.Terms[i].Coefficient, value.Terms[i].Degree - 1);
+            var result = new Polynomial(terms);
+            return result;
+        }
+
+        public static Polynomial operator -(Polynomial value)
+        {
+            return Negate(value);
+        }
+
+        public static Polynomial operator +(Polynomial value)
+        {
+            return value;
+        }
+
+        public static Polynomial operator +(Polynomial left, Polynomial right)
+        {
+            return Add(left, right);
+        }
+
+        public static Polynomial operator +(Polynomial left, PolynomialTerm right)
+        {
+            return Add(left, new Polynomial(right));
+        }
+
+        public static Polynomial operator +(PolynomialTerm left, Polynomial right)
+        {
+            return Add(new Polynomial(left), right);
+        }
+
+        public static Polynomial operator -(Polynomial left, Polynomial right)
+        {
+            return Subtract(left, right);
+        }
+
+        public static Polynomial operator -(Polynomial left, PolynomialTerm right)
+        {
+            return Subtract(left, new Polynomial(right));
+        }
+
+        public static Polynomial operator -(PolynomialTerm left, Polynomial right)
+        {
+            return Subtract(new Polynomial(left), right);
+        }
+
+        public static Polynomial operator *(Polynomial left, Polynomial right)
+        {
+            return Multiply(left, right);
+        }
+
+        public static Polynomial operator *(Polynomial left, PolynomialTerm right)
+        {
+            return Multiply(left, new Polynomial(right));
+        }
+
+        public static Polynomial operator *(PolynomialTerm left, Polynomial right)
+        {
+            return Multiply(new Polynomial(left), right);
+        }
+
+        public static Polynomial operator *(Polynomial left, Rational right)
+        {
+            return Multiply(left, new Polynomial(right));
+        }
+
+        public static Polynomial operator *(Rational left, Polynomial right)
+        {
+            return Multiply(new Polynomial(left), right);
+        }
+
+        public static Polynomial operator /(Polynomial left, Radical right)
+        {
+            return Divide(left, right);
+        }
+
+        public RadicalSum EvaluateAt(RadicalSum value)
+        {
+            RadicalSum result = RadicalSum.Zero;
+            for (int i = 0; i < Terms.Length; i++)
+            {
+                RadicalSum termValue = Terms[i].Coefficient;
+                for (int j = 0; j < Terms[i].Degree; j++)
+                    termValue *= value;
+                result += termValue;
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Radicals/Polynomials/PolynomialTerm.cs
@@ -0,0 +1,113 @@
+﻿using Rationals;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Radicals.Polynomials
+{
+    internal readonly struct PolynomialTerm
+        : IEquatable<PolynomialTerm>
+    {
+        private readonly int _degree;
+        private readonly RadicalSum _coefficient;
+        public int Degree
+        {
+            get
+            {
+                return _degree;
+            }
+        }
+        public RadicalSum Coefficient
+        {
+            get
+            {
+                if (_coefficient == null)
+                    return RadicalSum.Zero;
+                return _coefficient;
+            }
+        }
+
+        public PolynomialTerm(RadicalSum constant)
+            : this(constant, 0)
+        {
+        }
+
+        public PolynomialTerm(RadicalSum coefficient, int degree)
+        {
+            if (coefficient == null)
+                throw new ArgumentNullException(nameof(coefficient));
+            if (degree < 0)
+                throw new ArgumentException("Degree cannot be negative", nameof(degree));
+            _coefficient = coefficient;
+            if (coefficient == RadicalSum.Zero)
+                _degree = 0;
+            else
+                _degree = degree;
+        }
+
+        public static PolynomialTerm Zero = new PolynomialTerm(RadicalSum.Zero, 0);
+        public static PolynomialTerm One = new PolynomialTerm(RadicalSum.One, 0);
+        
+
+        public static PolynomialTerm Negate(PolynomialTerm value)
+        {
+            var result = new PolynomialTerm(-value.Coefficient, value.Degree);
+            return result;
+        }
+
+        public static Polynomial Add(PolynomialTerm left, PolynomialTerm right)
+        {
+            return new Polynomial(new PolynomialTerm[2] { left, right });
+        }
+
+        public static Polynomial Subtract(PolynomialTerm left, PolynomialTerm right)
+        {
+            return Add(left, -right);
+        }
+
+        public static PolynomialTerm Multiply(PolynomialTerm left, PolynomialTerm right)
+        {
+            return new PolynomialTerm(left.Coefficient * right.Coefficient, left.Degree + right.Degree);
+        }
+
+        public bool Equals(PolynomialTerm other)
+        {
+            return Degree == other.Degree && Coefficient == other.Coefficient;
+        }
+
+        public static PolynomialTerm operator -(PolynomialTerm value)
+        {
+            return Negate(value);
+        }
+
+        public static PolynomialTerm operator +(PolynomialTerm value)
+        {
+            return value;
+        }
+
+        public static Polynomial operator +(PolynomialTerm left, PolynomialTerm right)
+        {
+            return Add(left, right);
+        }
+
+        public static Polynomial operator -(PolynomialTerm left, PolynomialTerm right)
+        {
+            return Subtract(left, right);
+        }
+
+        public static PolynomialTerm operator *(PolynomialTerm left, PolynomialTerm right)
+        {
+            return Multiply(left, right);
+        }
+
+        public static bool operator ==(PolynomialTerm left, PolynomialTerm right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(PolynomialTerm left, PolynomialTerm right)
+        {
+            return !left.Equals(right);
+        }
+    }
+}
